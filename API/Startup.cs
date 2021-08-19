d790e19966b6fc7aea0742299976c23e71621fc7
Tihomir Ciric <ticaspeedy@gmail.com>
2021-08-19 17:22:39 +0200

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,13 +40,13 @@ namespace API
         {
             
            services.AddApplicationServices(_config);
-            services.AddControllers();
-            services.AddCors();
+           services.AddControllers();
+           services.AddCors();
            services.AddIdentityServices(_config);
-            // services.AddSwaggerGen(c =>
-            // {
-            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            // });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { User } from '../_models/User';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  
+  @Output() cancelRegister = new EventEmitter();
+  model:any ={};
+  
+  
+  constructor(private accountService:AccountService, private toastr:ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+  register()
+  {
+
+    // console.log(this.model.username)
+    // console.log(this.model.password)
+
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    }, err => {
+      console.log(err.error);
+      this.toastr.error(err.error);
+    })
+  }
+
+  cancel()
+  {
+    this.cancelRegister.emit(false);
+  }
+}
