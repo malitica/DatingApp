9b5fb1b5660dd17303c18c10435f317c222ee9b6
Tihomir Ciric <ticaspeedy@gmail.com>
2021-08-23 16:38:50 +0200

    End of section 8

---
+++ API/Data/Migrations/20210823100540_PhotosPublicId.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class PhotosPublicId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Photos",
+                newName: "PublicId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublicId",
+                table: "Photos",
+                newName: "MyProperty");
+        }
+    }
+}
